17694be836985165aabad653b0b0ba574cc2da6c
Fernando Aires Castello <fernandoairescastello@gmail.com>
2024-07-07 18:08:36 -0300

    View history

---
+++ UniqueColorExtractor/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace UniqueColorExtractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnLoadImage = new System.Windows.Forms.Button();
+            this.TxtColors = new System.Windows.Forms.TextBox();
+            this.LblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnLoadImage
+            // 
+            this.BtnLoadImage.Location = new System.Drawing.Point(12, 12);
+            this.BtnLoadImage.Name = "BtnLoadImage";
+            this.BtnLoadImage.Size = new System.Drawing.Size(107, 45);
+            this.BtnLoadImage.TabIndex = 0;
+            this.BtnLoadImage.Text = "Load Image";
+            this.BtnLoadImage.UseVisualStyleBackColor = true;
+            this.BtnLoadImage.Click += new System.EventHandler(this.BtnLoadImage_Click);
+            // 
+            // TxtColors
+            // 
+            this.TxtColors.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtColors.Location = new System.Drawing.Point(12, 63);
+            this.TxtColors.Multiline = true;
+            this.TxtColors.Name = "TxtColors";
+            this.TxtColors.ReadOnly = true;
+            this.TxtColors.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtColors.Size = new System.Drawing.Size(389, 375);
+            this.TxtColors.TabIndex = 1;
+            this.TxtColors.WordWrap = false;
+            // 
+            // LblMessage
+            // 
+            this.LblMessage.AutoSize = true;
+            this.LblMessage.Location = new System.Drawing.Point(143, 28);
+            this.LblMessage.Name = "LblMessage";
+            this.LblMessage.Size = new System.Drawing.Size(156, 13);
+            this.LblMessage.TabIndex = 2;
+            this.LblMessage.Text = "Press button to load an image...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 450);
+            this.Controls.Add(this.LblMessage);
+            this.Controls.Add(this.TxtColors);
+            this.Controls.Add(this.BtnLoadImage);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnLoadImage;
+        private System.Windows.Forms.TextBox TxtColors;
+        private System.Windows.Forms.Label LblMessage;
+    }
+}
+
---
+++ UniqueColorExtractor/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniqueColorExtractor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLoadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            var image = Image.FromFile(dialog.FileName) as Bitmap;
+            var colors = new HashSet<Color>();
+
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    Color color = image.GetPixel(x, y);
+                    colors.Add(color);
+                }
+            }
+
+            TxtColors.Clear();
+
+            foreach (Color color in colors)
+            {
+                string rgb = color.ToArgb().ToString("X06").Substring(2);
+                TxtColors.Text += rgb + Environment.NewLine;
+            }
+
+            LblMessage.Text = "Unique colors: " + colors.Count;
+        }
+    }
+}
